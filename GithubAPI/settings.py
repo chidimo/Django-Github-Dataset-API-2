de158319020de32d36c54878ce9a64733c3fb25b
Chidi Orji <orjichidi95@gmail.com>
2019-06-14 19:35:37 +0100

    Add solution files

--- GithubAPI/settings.py
+++ GithubAPI/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_URL = "http://localhost:8000"
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'RestAPI'
+    'RestAPI',
+    'django_extensions',
+
 ]
 
 MIDDLEWARE = [
--- RestAPI/models.py
+++ RestAPI/models.py
@@ -1,2 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.shortcuts import reverse
+from django.conf import settings
+
+from django.db import models
+
+
+class Actor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    login = models.CharField(max_length=50)
+    avatar_url = models.URLField()
+    event_count = models.IntegerField(default=0)
+    latest_event_timestamp = models.DateTimeField(blank=True, null=True)
+    streak = models.IntegerField(default=0)
+    pushed_today = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-event_count', '-latest_event_timestamp', 'login')
+
+    def __str__(self):
+        return self.login
+
+    def actor_events_queryset(self):
+        return self.event_set.all().order_by('-created_at', )
+
+    def actor_events_url(self):
+        return settings.BASE_URL + reverse('RestAPI:actor_events', args=[self.id])
+
+class Repo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=12)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        ordering = ("id", )
+
+    def __str__(self):
+        return "{}".format(self.id)
