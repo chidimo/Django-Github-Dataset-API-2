5c48588ca9c98f88aee598133cca587df8f640c0
Chidi Orji <orjichidi95@gmail.com>
2019-06-16 02:24:55 -0700

    refactor: simplify function logic

--- RestAPI/management/commands/create_one.py
+++ RestAPI/management/commands/create_one.py
@@ -1,6 +1,7 @@
 import os
 import json
 import glob
+from random import choice
 
 import django
 from django.conf import settings
@@ -12,10 +13,9 @@ DATA_STORE = os.path.join(settings.BASE_DIR, 'TestData')
 data_files = glob.glob('{}/*.json'.format(DATA_STORE))
 
 class Command(BaseCommand):
-    help = 'Create events data'
+    help = 'Create single event data'
 
     def handle(self, *args, **options):
-
         data = []
 
         for each in data_files:
@@ -25,11 +25,11 @@ class Command(BaseCommand):
                     if js['request']['method'] == "POST": 
                         data.append(json.loads(line))
 
-        for item in data:
-            body = item['request']['body']
-            message, status = create_events_data(body)
-            if status == 201:
-                self.stdout.write(self.style.SUCCESS(status))
-            else:
-                self.stdout.write(self.style.ERROR(status))
+        item = choice(data)
+        body = item['request']['body']
+        message, status = create_events_data(body)
+        if status == 201:
+            self.stdout.write(self.style.SUCCESS(status))
+        else:
+            self.stdout.write(self.style.ERROR(status))
         self.stdout.write(self.style.SUCCESS('Finished creating data'))
